28768badf131fa3682aac0d2cffa9deeba1de542
Robin <robin.noormets@voco.ee>
2023-02-01 15:16:31 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 
-const path = require("path")
+const path = require('path')
 
-const hbs = require('express-handlebars');
-
+const hbs = require('express-handlebars')
 app.set('views', path.join(__dirname, 'views'));
-app.set("view engine", 'hbs');
+app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
-    extname: 'hbs',
-    defaultLayout: "main",
-    layoutsDir: __dirname + "/views/layouts",
+    extname:'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
 }))
 
 app.use(express.static('public'));
 
-const mysql = require("mysql")
-
-const bodyParser = require("body-parser")
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-let con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "qwerty",
-    database: "joga_mysql"
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'joga_mysql',
 })
 
-con.connect(function (err) {
-    if (err) throw err
-    console.log("Connected to joga_mysql db")
+con.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to joga_mysql database successfully!')
+})
+
+app.get('/', (req,res) =>{
+    let query = 'SELECT * FROM article';
+    let articles = []
+    con.query(query, (err,result)=>{
+        if(err) throw err;
+        articles = result
+        res.render('index', {
+            articles: articles
+        })
+    })
 })
 
 app.listen(3000, () => {
-    console.log("App is started at http://localhost:3000")
+    console.log('App is started')
 })
