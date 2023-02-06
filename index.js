6fdeb460c58cd6ec5132216f52c08d08293c9971
Robin <robin.noormets@voco.ee>
2023-02-06 15:40:16 +0200

    Displayed an article by slug

--- index.js
+++ index.js
@@ -43,6 +43,19 @@ app.get('/', (req,res) =>{
     })
 })
 
+app.get('/article/:slug', (req,res) =>{
+    let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`
+    let article
+    con.query(query, (err, result) =>{
+        if(err) throw err;
+        article = result
+        console.log(article)
+        res.render('article',{
+            article:article
+        })
+    })
+})
+
 app.listen(3000, () => {
     console.log('App is started')
 })
